57991329650557f717373b6a4df96b7bdb28333c
Netsai <hellonetsai@gmail.com>
2025-02-17 18:44:30 +0200

    IndexOf and contains ArrayList methods

---
+++ Loops/Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+
+		ArrayList<Double>al = new ArrayList<>();
+
+		System.out.println(al.indexOf(1.0));
+ 		System.out.println(al.contains("string"));	
+}
+}
