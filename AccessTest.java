0f9cd6785adc2a2b4c009b18c3d109ec98fa9f63
Netsai <hellonetsai@gmail.com>
2025-02-18 04:13:30 +0200

    Access the class instannce in the inner class

---
+++ AccessTest.java
@@ -0,0 +1,21 @@
+public class AccessTest{
+	String a = "x";
+	static char b = 'x';
+	String c = "x";
+
+class Inner{
+	String a = "y";
+	String get(){
+
+	String c = "temp";
+	c = ""+AccessTest.b;
+	return c;
+		}
+	}
+AccessTest() {
+
+	System.out.println( new Inner().get() );
+	}
+public static void main(String args[])
+{new AccessTest();}
+}
