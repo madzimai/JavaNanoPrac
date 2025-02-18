c332888078278b13d4ba2e586711826fdf7b82f7
Netsai <hellonetsai@gmail.com>
2025-02-18 03:34:18 +0200

    Value of character annd way to convert it to string - char is converted to string if stirng is first arguement

--- Loops/Test.java
+++ Loops/Test.java
@@ -6,6 +6,10 @@ public class Test {
 		ArrayList<Double>al = new ArrayList<>();
 
 		System.out.println(al.indexOf(1.0));
- 		System.out.println(al.contains("string"));	
+ 		System.out.println(al.contains("string"));
+		System.out.println('b'+new Integer(63));
+
+		System.out.println("a"+'b'+69);
+		System.out.println('m'+new Integer(7));	
 }
 }
