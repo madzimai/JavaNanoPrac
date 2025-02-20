5e7ac232dd01e8b6c666053893b036312d8c581f
Netsai <hellonetsai@gmail.com>
2025-02-20 16:09:17 +0200

    Access static field with class of object returned by method

---
+++ DatabaseWrapper.java
@@ -0,0 +1,13 @@
+public class DatabaseWrapper
+{
+  static String url = "jdbc://derby://localhost:1527//mydb";
+  static DatabaseWrapper getDatabase()
+  {
+     System.out.println("Getting DB");
+     return null;
+  }
+  public static void main(String[ ] args)
+  {
+    System.out.println( getDatabase().url );
+  }
+}
