d2bd6cafd3ec937855429c85786cc7d90888fd49
Netsai <hellonetsai@gmail.com>
2025-02-20 16:29:56 +0200

    Fail to access non static members in main method

--- DatabaseWrapper.java
+++ DatabaseWrapper.java
@@ -1,5 +1,7 @@
 public class DatabaseWrapper
+
 {
+ int x;
   static String url = "jdbc://derby://localhost:1527//mydb";
   static DatabaseWrapper getDatabase()
   {
@@ -9,5 +11,7 @@ public class DatabaseWrapper
   public static void main(String[ ] args)
   {
     System.out.println( getDatabase().url );
+  //  System.out.println( this.x );non-static members cannot be access direct 
+//in static main method
   }
 }
