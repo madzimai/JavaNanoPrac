619fba2357a1d7b47e98f258b8a79a33b495796e
Netsai <hellonetsai@gmail.com>
2025-02-21 21:54:59 +0200

    Basic java class

---
+++ Basics.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Basics {
+static  String name ="Netsai  ";
+
+	public static void main (String [] args){
+	int x = 10;
+     System.out.println (name + x);
+
+	}
+}
