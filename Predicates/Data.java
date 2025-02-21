4ec13d76b143847e6ff2881ef11bb41c04b47183
Netsai <hellonetsai@gmail.com>
2025-02-21 12:11:57 +0200

    Use lambda expression to remove even numbers

---
+++ Predicates/Data.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Data{
+    int value;
+    Data(int value){
+        this.value = value;
+    }
+    public String toString(){ return ""+value; }
+    
+    public static void main(String[] args) {
+        Data[] dataArr = { new Data(1), new Data(2),
+              new Data(3), new Data(4) };
+
+        List<Data> dataList = new ArrayList<>( Arrays.asList(dataArr)); //1
+
+      
+            dataList.removeIf( d -> {
+		boolean isEven = d.value % 2 == 0;
+
+ 		if(isEven){
+		System.out.println("Removed" + d);
+		}
+		return isEven;
+
+	});
+
+            System.out.println("Remaing list " +dataList +", "); 
+        
+   }      
+}
