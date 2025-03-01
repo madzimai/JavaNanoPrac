66b98ca0a7150e03337a460b70311e43035b0b05
Netsai <hellonetsai@gmail.com>
2025-03-01 23:16:47 +0200

    Data types and loops

--- DataTypes/ReferenceVsPrimitive.java
+++ DataTypes/ReferenceVsPrimitive.java
@@ -7,7 +7,7 @@ public class ReferenceVsPrimitive {
         String text1 = "Hello";  // Reference variable stores address
         String text2 = text1;     // Both refer to same object
         text2 = "World";  // text2 now refers to a different object
-        
+	System.out.println(10 - 3 + 2 * 5);        
         System.out.println("num1: " + num1 + ", num2: " + num2);
         System.out.println("text1: " + text1 + ", text2: " + text2);
 
---
+++ Loops/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;  // Initialization
+
+        while (i <= 5) {  // Condition
+            System.out.println("Count: " + i);
+            i++;  // Increment
+        }
+    }
+}
+
